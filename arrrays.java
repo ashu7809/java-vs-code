9b429e21eb4876a223489fd25fdcf71b01832f46
ashu7809 <128628139+ashu7809@users.noreply.github.com>
2023-10-24 02:29:33 +0530

    Add files via upload

---
+++ arrrays.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class arrrays {
+    public static void main(String[] args) {
+      //store a roll number
+    //  int a=19;
+   //store a person name
+  // String name="kunal Kuassh0";
+
+   //syntax
+   //dataType[] variable_name=new DataType[size]
+//    int [] roln1=new int [5];
+//    //or directly
+//    int [] rnos={23,34,2,3,2};
+ 
+
+      int []ros;// declaration of array.ros is getting defined in the stack
+    ros=new int[5];//actually here obect is being created in the memory(heap)
+    System.out.println(ros[4]);
+    String [] arr=new String[4];
+    System.out.println(arr[0]);
+
+
+
+    }
+}
